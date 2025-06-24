209901dab9651ac4bdc9959660c27c0f98f4dacd
Emin Alakbarli <eminworkhub@gmail.com>
2025-06-24 19:04:53 +0400

    Python command

---
+++ Assign Python variables/example.py
@@ -0,0 +1,11 @@
+# Assign variables of different types
+my_int = 10
+my_float = 3.14
+my_string = "Emin"
+my_bool = True
+
+# Print variables and their types
+print(my_int, type(my_int))
+print(my_float, type(my_float))
+print(my_string, type(my_string))
+print(my_bool, type(my_bool))
